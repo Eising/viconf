d4ac74fc794ab850700b7a8674904edb565e6f2f
Allan Eising <allan@eising.dk>
2018-07-04 10:17:29 +0200

    Added bootstrap theme

---
+++ nodes/static/js/custom.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+
+
+  $(".submenu > a").click(function(e) {
+    e.preventDefault();
+    var $li = $(this).parent("li");
+    var $ul = $(this).next("ul");
+
+    if($li.hasClass("open")) {
+      $ul.slideUp(350);
+      $li.removeClass("open");
+    } else {
+      $(".nav > li > ul").slideUp(350);
+      $(".nav > li").removeClass("open");
+      $ul.slideDown(350);
+      $li.addClass("open");
+    }
+  });
+
+});
+
+$('#confirm-delete').on('show.bs.modal', function(e) {
+    $(this).find('.btn-ok').attr('href', $(e.relatedTarget).data('href'));
+});
